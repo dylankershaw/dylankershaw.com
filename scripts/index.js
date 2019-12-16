12e1f5a23ca6fffda4c30cc35cf14d30fd71bef2
dylankershaw <dylankershaw@me.com>
2019-12-16 17:28:46 -0500

    remove duplicate elements when done scrolling

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
-function trippyScroll() {
+const asyncTimeout = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function trippyScroll() {
   const el = document.getElementsByClassName('main-page')[0];
   const parent = el.parentElement;
 
@@ -10,13 +12,19 @@ function trippyScroll() {
 
   appendEl(50);
 
-  setTimeout(() => {
-    easyScroll({
-      scrollableDomEle: window,
-      direction: 'bottom',
-      duration: 10000,
-      easingPreset: 'easeInOutQuad',
-      scrollAmount: document.body.scrollHeight
-    });
-  }, 250);
+  await asyncTimeout(250);
+
+  easyScroll({
+    scrollableDomEle: window,
+    direction: 'bottom',
+    duration: 10000,
+    easingPreset: 'easeInOutQuad',
+    scrollAmount: document.body.scrollHeight
+  });
+
+  await asyncTimeout(10000);
+
+  document.querySelectorAll('.main-page').forEach((el, i) => {
+    if (i > 0) el.remove();
+  });
 }
