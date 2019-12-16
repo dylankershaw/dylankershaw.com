fd68013f139b20b225c21517b5c973b30c463fce
dylankershaw <dylankershaw@me.com>
2019-12-16 17:17:24 -0500

    this is 100000x better

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,22 @@
-const asyncTimeout = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-async function trippyScroll() {
+function trippyScroll() {
   const el = document.getElementsByClassName('main-page')[0];
-  const elClone = el.cloneNode(true);
   const parent = el.parentElement;
 
-  async function handleEl() {
-    el.classList.remove('main-page--center');
-    console.log('sending up el');
-    await asyncTimeout(5000);
-    swipeUp(el, 5000);
+  function appendEl(n) {
+    if (n === 0) return;
+    parent.appendChild(el.cloneNode(true));
+    appendEl(n - 1);
   }
 
-  async function handleElClone() {
-    elClone.classList.add('main-page--222');
-    elClone.classList.add('main-page--below');
-    elClone.classList.remove('main-page--center');
-    parent.appendChild(elClone);
+  appendEl(50);
 
-    await asyncTimeout(10);
-    elClone.classList.remove('main-page--below');
-  }
-
-  handleEl();
-  // handleElClone();
-}
-
-async function swipeUp(el, ms) {
-  if (ms <= 10) return;
-  el.classList.add('main-page--below');
-  el.style.transitionDuration = ms / 1000 + 's';
-  await asyncTimeout(10);
-  el.classList.remove('main-page--below');
-  await asyncTimeout(ms);
-  swipeUp(el, ms / 1.5);
+  setTimeout(() => {
+    easyScroll({
+      scrollableDomEle: window,
+      direction: 'bottom',
+      duration: 10000,
+      easingPreset: 'easeInOutQuad',
+      scrollAmount: document.body.scrollHeight
+    });
+  }, 250);
 }
