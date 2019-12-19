ae10d9b5d90d5a59a4291bc8724fb658afa4dc79
Dylan Kershaw <dylankershaw@me.com>
2019-12-19 00:04:56 -0500

    add CLI

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', setVh);
+document.addEventListener('keydown', handleKeyDown);
 
 function setVh() {
   const vh = window.innerHeight * 0.01;
@@ -6,6 +7,48 @@ function setVh() {
 }
 
 const asyncTimeout = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+function handleKeyDown() {
+  const input = document.getElementsByClassName(
+    'main-page__command-line-input'
+  )[0];
+
+  switch (event.key) {
+    case 'Backspace':
+      input.innerText = input.innerText.slice(0, input.innerText.length - 1);
+      break;
+    case 'Enter':
+      const command = input.innerText;
+      input.innerText = '';
+      handleSubmit(command);
+      break;
+    default:
+      const inputIsValid =
+        input.innerText.length <= 32 &&
+        event.key.length === 1 &&
+        event.key.match(/\w/g);
+
+      if (inputIsValid) input.innerText += event.key;
+  }
+}
+
+function handleSubmit(command) {
+  const responseEl = document.getElementsByClassName(
+    'main-page__command-line-response'
+  )[0];
+
+  responseEl.innerText = '';
+
+  switch (command) {
+    case '':
+      break;
+    case 'scroll':
+      trippyScroll();
+      break;
+    default:
+      responseEl.innerText = command + ': command not found';
+  }
+}
 
 async function trippyScroll() {
   const el = document.getElementsByClassName('main-page')[0];
@@ -34,6 +77,4 @@ async function trippyScroll() {
   document.querySelectorAll('.main-page').forEach((el, i) => {
     if (i > 0) el.remove();
   });
-
-  // TODO: land on a new page
 }
